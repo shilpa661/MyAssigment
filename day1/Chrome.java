48c1f3263b763af08d40749a430c7bcb208d3b3c
shilpa661 <shilpaselin29@gmail.com>
2024-10-17 18:56:36 +0530

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float Version = 91.0f;
+		String developer = "Google";
+		Boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+	System.out.println("Version is = " +Version);
+	System.out.println("developer is = " +developer);
+System.out.println( "isBeta is = "+isBeta);
+System.out.println("releaseYear is = " +releaseYear);
+System.out.println( "shortcutKey is =  "+shortcutKey);
+
+
+		
+		
+		
+		
+		
+		
+		
+				
+				
+				
+
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		String browserName ="firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo ='f';
+System.out.println("Browser Version is = "+browserVersion);
+System.out.println("Browser Name is = "+browserName);
+System.out.println("InVisible is = "+isVisible);
+System.out.println("Release Year is = "+releaseYear);
+System.out.println("Browser Logo is = "+browserLogo);
+
+
+
+		
+				
+				
+
+	}
+
+}
